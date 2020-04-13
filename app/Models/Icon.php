2882641f7ad31f038d5ac89c7a24812cb00cdef1
Awilum <awilum@msn.com>
2020-04-13 21:51:09 +0300

    feat(core): general codebase updates

--- app/Models/Icon.php
+++ app/Models/Icon.php
@@ -15,29 +15,35 @@ use Flextype\Component\Filesystem\Filesystem;
 
 class Icon extends Container
 {
-    public static function icon($value)
+    public static function icon($value = null) : string
     {
-        $icon_parts = explode(" ", $value);
+        $icon_fallback_file_path = PATH['site'] . '/plugins/' . '/icon/assets/dist/fontawesome/svgs/regular/file-alt.svg';
 
-        if ($icon_parts[0] == 'fas') {
-            $icon_category = 'solid';
-        } elseif ($icon_parts[0] == 'fab') {
-            $icon_category = 'brands';
-        } elseif ($icon_parts[0] == 'far') {
-            $icon_category = 'regular';
-        } else {
-            $icon_category = 'regular';
-        }
-
-        $icon_name = str_replace("fa-", "", $icon_parts[1]);
-
-        $icon_file_path = PATH['plugins'] . '/icon/assets/dist/fontawesome/svgs/' . $icon_category . '/' . $icon_name . '.svg';
-        $icon_fallback_file_path = PATH['plugins'] . '/icon/assets/dist/fontawesome/svgs/regular/file-alt.svg';
-
-        if (Filesystem::has($icon_file_path)) {
-            $icon = Filesystem::read($icon_file_path);
-        } else {
+        if ($value == null) {
             $icon = Filesystem::read($icon_fallback_file_path);
+        } else {
+
+            $icon_parts = explode(" ", $value);
+
+            if ($icon_parts[0] == 'fas') {
+                $icon_category = 'solid';
+            } elseif ($icon_parts[0] == 'fab') {
+                $icon_category = 'brands';
+            } elseif ($icon_parts[0] == 'far') {
+                $icon_category = 'regular';
+            } else {
+                $icon_category = 'regular';
+            }
+
+            $icon_name = str_replace("fa-", "", $icon_parts[1]);
+
+            $icon_file_path = PATH['site'] . '/plugins/' . '/icon/assets/dist/fontawesome/svgs/' . $icon_category . '/' . $icon_name . '.svg';
+
+            if (Filesystem::has($icon_file_path)) {
+                $icon = Filesystem::read($icon_file_path);
+            } else {
+                $icon = Filesystem::read($icon_fallback_file_path);
+            }
         }
 
         return $icon;
